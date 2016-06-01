18ac4ec9a422ec6b2a0efca706d2c5d58e1a909b
Mike Crute <mcrute@gmail.com>
2016-05-31 21:51:33 -0700

    Stub in VLC back-end

--- pydora/backend.py
+++ pydora/backend.py
@@ -61,3 +61,52 @@ class MPG123BackEnd(object):
         """
         if self._player_is_stopped(message):
             raise StopIteration
+
+
+# /Applications/VLC.app/Contents/MacOS/VLC \
+#       --intf http --http-host 127.0.0.1 --http-port 9090 --http-password 123
+
+class VLCBackEnd(object):
+    """Remote control for a VLC process
+
+    Starts and owns a handle to an VLC process then feeds commands to it to
+    play pandora audio
+    """
+
+    def __init__(self):
+        self._process = None
+
+    def __del__(self):
+        """Cleanup player process on destruction
+        """
+        self._process.kill()
+
+    def ensure_started(self, interesting_fds):
+        """Ensure mpg123 is started
+        """
+        if self._process and self._process.poll() is None:
+            return
+
+        self._process = SilentPopen(
+            ["mpg123", "-q", "-R", "--ignore-mime"])
+
+    def stop(self):
+        """Stop the currently playing song
+        """
+        pass
+
+    def pause(self):
+        """Pause the player
+        """
+        pass
+
+    def play(self, url):
+        """Play the player
+        """
+        pass
+
+    def handle_message(self, message):
+        """Handle a message from the player back-end
+        """
+        if self._player_stopped(message):
+            raise StopIteration
